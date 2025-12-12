03db84db8c1083b826c818d6e21cf8754d39607c
Noah07 <lambertn599@gmail.com>
2025-12-12 09:34:33 +0100

    q

--- js/script.js
+++ js/script.js
@@ -13,7 +13,6 @@ function includeHTML(selector, file) {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  includeHTML("#navbarindex", "partials/indexNavbar.html");
   includeHTML("#navbar", "partials/navbar.html");
   includeHTML("#footer", "partials/footer.html");
 });
