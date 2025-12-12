f699cf0a8dc1fe9b11f52b48cde291bc37e467df
Noah07 <lambertn599@gmail.com>
2025-12-12 09:22:18 +0100

    df

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function includeHTML(selector, file) {
+  fetch(file)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Erreur chargement ${file}`);
+      }
+      return response.text();
+    })
+    .then(data => {
+      document.querySelector(selector).innerHTML = data;
+    })
+    .catch(error => console.error(error));
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  includeHTML("#navbar", "partials/navbar.html");
+  includeHTML("#footer", "partials/footer.html");
+});
